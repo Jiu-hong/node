f961ed149c7b5ad89950f1504cd410a631b7150e
sjh <jiu.hong.sun@gmail.com>
2021-03-02 15:45:16 +0800

    c

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const Cors = require("cors")
 const app = Express()
 //app.use(Cors())
 
-
+/*
 app.get("/" ,(request, response)=>{
     response.send("hello world")
-})
+})*/
 
 app.listen(4000,()=>{console.log("I am listening on 4000")})
